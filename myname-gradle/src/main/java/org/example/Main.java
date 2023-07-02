bc5fb817d5c39671d457cc0c45d91e8f3e9b77f8
olena0112 <124000255+olena0112@users.noreply.github.com>
2023-07-02 21:43:57 +0300

    hw1

---
+++ myname-gradle/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Olena", "Dobrova");
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+}
